3303bad6f7c46c607fa7b2cb90bade0501805930
Remarc Balisi <remarc.balisi@gmail.com>
2020-12-06 05:58:04 +0800

    add seeder for role and user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $this->adminRole = Role::create(['guard_name' => 'api', 'name' => 'admin']);
+        $this->customerRole = Role::create(['guard_name' => 'api', 'name' => 'customer']);
+        $this->admin = User::factory()->create([
+            'email' => 'admin@email.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+        ]);
+        $this->customer = User::factory()->create([
+            'email' => 'customer@email.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+        ]);
+
+        $this->admin->assignRole($this->adminRole);
+        $this->customer->assignRole($this->customerRole);
+
     }
 }
